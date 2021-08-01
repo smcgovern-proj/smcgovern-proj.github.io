fd10ee7ed843599b2a99663bb981dbe3238285da
Sean <smcgovern135@gmail.com>
2021-07-31 20:46:06 -0400

    fixed photo aspect

--- src/index.svelte
+++ src/index.svelte
@@ -3,8 +3,10 @@
 
 <main>
   <div class="sidebar">
-    <img src="http://placecorgi.com/260/180" alt="either me or a corgi">
-    <h1 class="name">Sean McGovern</h1>
+    <div class="photo">
+      <img src="./assets/profile-full-cropped.jpg" alt="either me or a corgi"/>
+    </div>
+    <h1 class="name">SEAN MCGOVERN</h1>
     <p class="bio">Hi! I'm a full-stack web developer and UX-dabbler based in Washington, D.C. I believe in constant self-improvement, in design/dev as a means of advocating for the end user, and in always seasoning your pasta water. Connect with me below:</p>
   </div>
   <div class="body">
@@ -13,19 +15,24 @@
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&display=swap');
+
   :root {
     --font-main: Lato;
   }
+
   * {
     box-sizing: border-box;
-    margin: 0;
-    padding: 0
   }
+
   main {
    display: grid;
    grid-template-columns: minmax(150px, 25%) 1fr;
   }
+  .sidebar {
+    margin-left: 1rem;
+    margin-top: 1rem;
 
+  }
   .body {
     display: flex;
     flex-flow: column nowrap;
@@ -43,4 +50,13 @@
     font-family: var(--font-main);
     font-weight: 400;
   }
+
+  .photo {
+    width: 273px;
+    height: 273px;
+  }
+  .photo img {
+    width: 100%;
+  }
+
 </style>
